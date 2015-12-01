f9dcb630fa8efb3149aaa87521de6c7e728c7782
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-12-01 16:40:09 +0100

    make deletion features simpler

--- nala/features/regex.py
+++ nala/features/regex.py
@@ -30,17 +30,8 @@ class RegexNLFeatureGenerator(FeatureGenerator):
             for sentence in part.sentences:
                 for token in sentence:
                     for match_index, match in matches.items():
-                        name = 'regex_nl_{}'.format(match_index)
-                        value = 'O'
                         for start, end in match:
-                            if start == token.start:
-                                value = 'B'
+                            if start <= token.start < token.end <= end:
+                                token.features['nl_deletion'] = 1
+                                print(token)
                                 break
-                            elif start < token.start < token.end < end:
-                                value = 'I'
-                                break
-                            elif token.end == end:
-                                value = 'E'
-                                break
-
-                        token.features[name] = value
