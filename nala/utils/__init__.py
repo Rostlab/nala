031afb6bbef331db1b95c0707bfa6b7c47998d67
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2015-12-22 14:13:45 +0100

    Add a helper method for best model

--- nala/utils/__init__.py
+++ nala/utils/__init__.py
@@ -15,3 +15,25 @@ PRO_REL_ORG_CLASS_ID = 'r_5'  # e_1/e_3
 THRESHOLD_VALUE = 1
 ENTREZ_GENE_ID = 'n_4'
 UNIPROT_ID = 'n_5'
+
+
+def get_prepare_pipeline_for_best_model():
+    """
+    Helper method that returns an instance of PrepareDatasetPipeline
+    which uses the best configuration for predicating mutation mentions.
+
+    :returns nalaf.structures.dataset_pipelines.PrepareDatasetPipeline
+    """
+    from nalaf.structures.dataset_pipelines import PrepareDatasetPipeline
+    from nalaf.features.simple import SimpleFeatureGenerator, SentenceMarkerFeatureGenerator
+    from nalaf.features.stemming import PorterStemFeatureGenerator
+    from nalaf.features.window import WindowFeatureGenerator
+    from nala.features.tmvar import TmVarFeatureGenerator, TmVarDictionaryFeatureGenerator
+
+    include = ['pattern0[0]', 'pattern1[0]', 'pattern2[0]', 'pattern3[0]', 'pattern4[0]', 'pattern5[0]',
+               'pattern6[0]', 'pattern7[0]', 'pattern8[0]', 'pattern9[0]', 'pattern10[0]', 'word[0]', 'stem[0]']
+    return PrepareDatasetPipeline(feature_generators=[SimpleFeatureGenerator(), PorterStemFeatureGenerator(),
+                                                      SentenceMarkerFeatureGenerator(),
+                                                      TmVarFeatureGenerator(), TmVarDictionaryFeatureGenerator(),
+                                                      WindowFeatureGenerator(template=(-3, -2, -1, 1, 2, 3),
+                                                                             include_list=include)])
