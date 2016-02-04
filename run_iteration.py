eed371c18e7959f49acd299f1b90bc63257521a7
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2016-02-04 10:26:38 +0100

    Add reviewed 18/19

--- run_iteration.py
+++ run_iteration.py
@@ -6,8 +6,8 @@ from nala.bootstrapping.iteration import Iteration
 import shutil
 import os
 
-username = 'abojchevski'
-password = 'noetic273490pi_eq'
+username = 'your user'
+password = 'your password'
 url = 'https://www.tagtog.net/api/0.1/documents'
 
 
@@ -39,7 +39,6 @@ def download(n):
     rev_dir = 'resources/bootstrapping/iteration_{}/reviewed'.format(n)
 
     ids = [file.replace('.html', '') for file in os.listdir(cnd_dir)]
-    ids = ['aQ35B8SGys7Xw_JScIWRxJLv10EK-14652006', 'a460KiRJuTQIzeIRFiaVCX.rArYu-16465590', 'agHSqKMlhEsyiJTsuSXnWrhZmLLS-10329126', 'a1YcD3kZ3gOLgXBBp1McwZ854AO0-9882455', 'aVKF88lizwGbpaOfBenn_uVpqxfq-21138967', 'aFsrilU8lsppsBAM6B8SyLYyT7Eu-18003854', 'aBG_a9P3EyqpYgdR7Grbtp2QHMR8-18644859', 'a6gc07qD0v87GjZQSITbIJ35cbLC-19648910', 'aLy36DKDscrq1guvcPr0Bpo.wmn4-8999926', 'avLEBqUVCkFk5hb4mAQphvA6xhXm-15107404']
 
     for tagtog_id in ids:
         params = {'project': 'nala', 'output': 'ann.json', 'owner': 'jmcejuela',
@@ -62,7 +61,7 @@ def validate(n):
     AnnJsonAnnotationReader(rev_dir).annotate(data)
     print(len(data))
 
-# run()
+run()
 # upload(22)
-download(23)
-validate(23)
+# download(22)
+# validate(22)
