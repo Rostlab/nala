0b7dbffc10dda1ec6ccad2d48c5a9750625f9139
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2016-03-07 12:17:57 +0100

    script to read any

--- scripts/get_corpus_stats.py
+++ scripts/get_corpus_stats.py
@@ -1,5 +1,6 @@
 import argparse
 from nala.bootstrapping.iteration import Iteration
+from nala.preprocessing.definers import ExclusiveNLDefiner
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Print corpora stats')
@@ -10,24 +11,27 @@ if __name__ == "__main__":
 
 dataset = None
 
-if args.dataset == "IDP4":
-    dataset = Iteration().read_IDP4()
-elif args.dataset == "nala":
-    dataset = Iteration().read_nala()
-elif args.dataset == "IDP4+":
-    dataset = Iteration().read_IDP4Plus()
-else:
-    raise Exception("Do not recognize given dataset name: " + args.dataset)
-
-print_stats(args.dataset, dataset)
-
-#----------
+#------------------------------------------------------------------------------
 
 def print_stats(name, dataset):
     counts=[0,0,0]
 
     ExclusiveNLDefiner().define(dataset)
-    for ann in dataset.train.annotations():
+    for ann in dataset.annotations():
         counts[ann.subclass] += 1
 
-    print(name, '#docs:', len(self.train.documents), 'ST:', counts[0], 'NL:', counts[1], 'SS:', counts[2], 'NL+SS:', counts[1] + counts[2])
+    print(name, '#docs:', len(dataset.documents), 'ST:', counts[0], 'NL:', counts[1], 'SS:', counts[2], 'NL+SS:', counts[1] + counts[2])
+
+
+#------------------------------------------------------------------------------
+
+if args.dataset == "IDP4":
+    dataset = Iteration.read_IDP4()
+elif args.dataset == "nala":
+    dataset = Iteration.read_nala()
+elif args.dataset == "IDP4+":
+    dataset = Iteration.read_IDP4Plus()
+else:
+    raise Exception("Do not recognize given dataset name: " + args.dataset)
+
+print_stats(args.dataset, dataset)
