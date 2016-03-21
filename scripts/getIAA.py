65d8d281695283ac6e793ef88530b8510449f069
Juan Miguel Cejuela <i@juanmi.rocks>
2016-03-21 21:36:32 +0100

    get IAA for IDP4

--- scripts/getIAA.py
+++ scripts/getIAA.py
@@ -10,12 +10,75 @@ from nala.preprocessing.definers import ExclusiveNLDefiner
 IDP4_members = ['Ectelion', 'abojchevski', 'sanjeevkrn', 'Shpendi']
 nala_members = ['cuhlig', 'abojchevski', 'jmcejuela']
 
+IDP4_IAA_docs = [
+"23144459",
+"9287304",
+"17728323",
+"9113409",
+"15546125",
+"6488317",
+"7559441",
+"9502794",
+"9688573",
+"12663043",
+"15718227",
+"12560877",
+"17327381",
+"15201367",
+"16621600",
+"10080891",
+"9026534",
+"PMC3244487",
+"15231984",
+"10454540",
+"7893703",
+"15015733",
+"8420982",
+"7912128",
+"PMC1143697",
+"9514978",
+"12019207",
+"PMC3464205",
+"7488859",
+"9101292",
+"PMC2233692",
+"8655141",
+"10843183",
+"PMC3815127",
+"8162033",
+"18199800",
+"9448270",
+"8013629",
+"10747788",
+"18179891",
+"18319072",
+"PMC31919",
+"11684697",
+"14576201",
+"11342563",
+"17468127",
+"PMC3030776",
+"14672928",
+"12559908",
+"PMC3668507",
+"9916143",
+"nar_gkp166",
+"ng.1091"]
+
 def benchmark_IDP4(member1, member2):
     dataset = Dataset()
     itr = IterationRound(0)
     dataset.extend_dataset(itr.read(read_annotations = False))
+
+    newcorpus = Dataset()
+    for docid, document in dataset.documents.items():
+        if docid in IDP4_IAA_docs:
+            newcorpus.documents[docid] = document
+
+    dataset = newcorpus
+
     AnnJsonAnnotationReader(os.path.join(itr.path, "base", "annjson", "members", member1), delete_incomplete_docs = True).annotate(dataset)
-    AnnJsonAnnotationReader(os.path.join(itr.path, "base", "annjson", "members", member1), delete_incomplete_docs = True, is_predicted=True).annotate(dataset)
+    AnnJsonAnnotationReader(os.path.join(itr.path, "base", "annjson", "members", member2), delete_incomplete_docs = True, is_predicted=True).annotate(dataset)
 
     print(dataset.__repr__())
 
