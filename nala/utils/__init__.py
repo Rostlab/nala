719e82764527bd14cbe078af21ee5800fd34698b
Juan Miguel Cejuela <i@juanmi.rocks>
2016-03-22 13:45:05 +0100

    use spacylemmatizer

--- nala/utils/__init__.py
+++ nala/utils/__init__.py
@@ -25,13 +25,14 @@ def get_prepare_pipeline_for_best_model():
     """
     from nalaf.structures.dataset_pipelines import PrepareDatasetPipeline
     from nalaf.features.simple import SimpleFeatureGenerator, SentenceMarkerFeatureGenerator
-    from nalaf.features.stemming import PorterStemFeatureGenerator
+    from nalaf.features.parsing import SpacyPosTagger
+    from nalaf.features.stemming import SpacyLemmatizer
     from nalaf.features.window import WindowFeatureGenerator
     from nala.features.tmvar import TmVarFeatureGenerator, TmVarDictionaryFeatureGenerator
 
     include = ['pattern0[0]', 'pattern1[0]', 'pattern2[0]', 'pattern3[0]', 'pattern4[0]', 'pattern5[0]',
                'pattern6[0]', 'pattern7[0]', 'pattern8[0]', 'pattern9[0]', 'pattern10[0]', 'word[0]', 'stem[0]']
-    return PrepareDatasetPipeline(feature_generators=[PorterStemFeatureGenerator(),
+    return PrepareDatasetPipeline(feature_generators=[SpacyLemmatizer(),
                                                       SentenceMarkerFeatureGenerator(),
                                                       TmVarFeatureGenerator(),
                                                       TmVarDictionaryFeatureGenerator(),
