031fb3aa58f2dae685a5eaf530e2ce56c5b649b3
abojchevski <aleksandar.bojchevski@gmail.com>
2016-03-22 15:44:02 +0100

    Spacy WE

--- scripts/train_embeddings.py
+++ scripts/train_embeddings.py
@@ -4,6 +4,7 @@ import re
 import os
 import multiprocessing
 from nltk import sent_tokenize
+from spacy.en import English
 
 """
 Script for training word embeddings using abstracts from the whole PubMed/Medline database
@@ -13,6 +14,7 @@ Script for training word embeddings using abstracts from the whole PubMed/Medlin
 class MedlineSentenceGenerator:
     def __init__(self, directory):
         self.directory = directory
+        self.nlp = English(parser=False, entity=False)
 
     @staticmethod
     def tokenize(sentence):
@@ -20,8 +22,15 @@ class MedlineSentenceGenerator:
         sentence = re.sub('([a-z])([A-Z])', r'\1 \2', sentence)
         sentence = re.sub('([A-Za-z])([0-9])', r'\1 \2', sentence)
         sentence = re.sub('([a-z])(fs)', r'\1 \2', sentence)
+        sentence = re.sub('([^\x00-\x7F])', r' \1 ', sentence)
         sentence = re.sub('([\W\-_])', r' \1 ', sentence)
+
         return sentence.lower().split()
+
+    def lemmatize(self, sentence):
+        spacy_doc = self.nlp.tokenizer.tokens_from_list(sentence)
+        self.nlp.tagger(spacy_doc)
+        return [spacy_token.lemma_ for spacy_token in spacy_doc]
 
     def __iter__(self):
         for root, _, files in os.walk(self.directory):
@@ -32,10 +41,10 @@ class MedlineSentenceGenerator:
 
                         for child in etree.parse(xml).getroot():
                             for title in child.iter('ArticleTitle'):
-                                yield self.tokenize(title.text)
+                                yield self.lemmatize(self.tokenize(title.text))
                             for abstract in child.iter('AbstractText'):
                                 for sentence in sent_tokenize(abstract.text):
-                                    yield self.tokenize(sentence)
+                                    yield self.lemmatize(self.tokenize(sentence))
                     except:
                         pass
 
@@ -44,7 +53,7 @@ def train():
     medline = MedlineSentenceGenerator('/mnt/project/rost_db/medline/')
 
     model = Word2Vec(medline, window=10, workers=multiprocessing.cpu_count(), sg=0)
-    model.save('/mnt/project/pubseq/nala/backup_we/medline_abstract_window_10_cbow_we.model')
+    model.save('/mnt/project/pubseq/nala/backup_we/spacy_we.model')
     print(model.total_train_time, len(model.vocab))
 
 
