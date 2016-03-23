60f37fec276fda4d25805571415802c2c6e01ef5
Juan Miguel Cejuela <i@juanmi.rocks>
2016-03-23 19:39:25 +0100

    minor

--- nala/learning/taggers.py
+++ nala/learning/taggers.py
@@ -92,7 +92,6 @@ class MultipleModelTagger(Tagger):
         os.rename(tmpdir + '/annjson', tmpdir + '/tagger2_annjson')
         self._clean_predictions(dataset, "tagger2")
 
-
         AnnJsonMergerAnnotationReader(tmpdir, strategy='union', entity_strategy='priority',
         priority=['tagger1_annjson', 'tagger2_annjson'],
         delete_incomplete_docs=False, is_predicted=True).annotate(dataset)
