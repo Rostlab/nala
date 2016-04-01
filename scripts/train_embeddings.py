8d6d902ba3bd9d11b13a91b40c1a70c826991187
Aleksandar Bojchevski <aleksandar.bojchevski@gmail.com>
2016-04-01 18:17:07 +0200

    add more flexibility for training

--- scripts/train_embeddings.py
+++ scripts/train_embeddings.py
@@ -3,10 +3,15 @@ from lxml import etree
 import re
 import os
 import multiprocessing
+
+from nalaf.preprocessing.spliters import NLTKSplitter
+from nalaf.preprocessing.tokenizers import TmVarTokenizer
 from nltk import sent_tokenize
 from spacy.en import English
 import sys
 import logging
+
+from scripts.train_idp4 import read_data
 
 """
 Script for training word embeddings using abstracts from the whole PubMed/Medline database
@@ -53,24 +58,48 @@ class MedlineSentenceGenerator:
                         pass
 
 
+class CorpusGenerator:
+    """
+    :type data: nalaf.structures.data.Dataset
+    """
+    def __init__(self):
+        self.data = read_data(51, True)
+        NLTKSplitter().split(self.data)
+        TmVarTokenizer().tokenize(self.data)
+
+    def __iter__(self):
+        for sentence in self.data.sentences():
+            yield [re.sub('\d', '0', token.word) for token in sentence]
+
+
 def train():
-    assert len(sys.argv) == 4
+    assert len(sys.argv) == 7
     training_folder = sys.argv[1]
     dimension = int(sys.argv[2])
     window_size = int(sys.argv[3])
+    is_sg = int(sys.argv[4])
+    num_iterations = int(sys.argv[5])
+    use_corpus = bool(sys.argv[6])
 
-    logging.basicConfig(filename='we_{}_{}.log'.format(dimension, window_size),
-                        format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    suffix = '{}_{}_{}_{}_{}'.format(dimension, window_size, is_sg, num_iterations, use_corpus)
+
+    # logging.basicConfig(filename='we_{}.log'.format(suffix),
+    #                     format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     logging.log(logging.INFO,
-                'start training a model with dimension {} and window size {}'.format(dimension, window_size))
+                'start training a model {}'.format(suffix))
 
-    medline = MedlineSentenceGenerator(training_folder)
+    if use_corpus:
+        dataset = CorpusGenerator()
+    else:
+        dataset = MedlineSentenceGenerator(training_folder)
 
-    model = Word2Vec(medline, window=window_size, workers=multiprocessing.cpu_count(), sg=0, size=dimension)
+    model = Word2Vec(dataset, window=window_size, workers=multiprocessing.cpu_count(), sg=is_sg, size=dimension,
+                     iter=num_iterations)
 
     model.init_sims(True)
-    model.save('/mnt/project/pubseq/nala/backup_we/number_replaced_{}_{}.model'.format(dimension, window_size))
+    model.save('/mnt/project/pubseq/nala/backup_we/number_replaced_{}.model'.format(suffix))
 
     print(model.total_train_time, len(model.vocab))
 
