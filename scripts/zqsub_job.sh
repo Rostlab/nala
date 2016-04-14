e27f814b15bcc1b18649e75be84ff71560d4d585
Juan Miguel Cejuela <i@juanmi.rocks>
2016-04-14 21:27:47 +0200

    last

--- scripts/zqsub_job.sh
+++ scripts/zqsub_job.sh
@@ -20,7 +20,7 @@ train_no_cv="time $python $trainscript --validation none --model_name_suffix $jo
 
 # FINAL MODELS
 
-# $train_cv --training_corpus nala_training --pruner parts --labeler BIEO --word_embeddings --we_additive 0 --we_multiplicative 1
+# FINAL BASELINE $train_cv --training_corpus nala_training --pruner parts --labeler BIEO --word_embeddings --we_additive 0 --we_multiplicative 1
 # $train_cv --training_corpus nala_training --pruner parts --labeler IO --word_embeddings --we_additive 0 --we_multiplicative 1
 # $train_cv --training_corpus tmVar_training --pruner parts --labeler IO --word_embeddings --we_additive 0 --we_multiplicative 1
 
