4cf4fb988e74ca711056fb5c252f9a2b49c90f2d
Juan Miguel Cejuela <i@juanmi.rocks>
2016-04-14 21:24:26 +0200

    add

--- scripts/zqsub_job.sh
+++ scripts/zqsub_job.sh
@@ -32,7 +32,7 @@ train_no_cv="time $python $trainscript --validation none --model_name_suffix $jo
 # FINAL BASELINE
 
 # branch with new PP juanmi improvements
-# $train_cv --training_corpus nala_training --pruner parts --labeler BIEO --word_embeddings --we_additive 0 --we_multiplicative 1
+$train_cv --training_corpus nala_training --pruner parts --labeler BIEO --word_embeddings --we_additive 0 --we_multiplicative 1
 
 # branch with and/or splitting but also accepting SS cases
 # $train_cv --training_corpus nala_training --pruner parts --labeler BIEO --word_embeddings --we_additive 0 --we_multiplicative 1
@@ -42,7 +42,7 @@ train_no_cv="time $python $trainscript --validation none --model_name_suffix $jo
 ## Alex experiments
 
 # develop -> new code
-$train_cv --training_corpus nala_training --pruner parts --labeler BIEO --word_embeddings --we_additive 0 --we_multiplicative 1
+# $train_cv --training_corpus nala_training --pruner parts --labeler BIEO --word_embeddings --we_additive 0 --we_multiplicative 1
 
 # feature_rule_0 -> no rule at all
 # $train_cv --training_corpus nala_training --pruner parts --labeler BIEO --word_embeddings --we_additive 0 --we_multiplicative 1
