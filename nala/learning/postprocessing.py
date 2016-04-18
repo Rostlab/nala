702f0d719a0ec149cf7f379aec9bd4183b2c9a11
Juan Miguel Cejuela <i@juanmi.rocks>
2016-04-18 18:03:56 +0200

    mkake bit more general

--- nala/learning/postprocessing.py
+++ nala/learning/postprocessing.py
@@ -47,7 +47,7 @@ class PostProcessing:
             re.compile(r'\b[ATCG][-+]*\d+[ATCG]/[ATCG]\b'),
 
             re.compile(r'\b[CISQMNPKDTFAGHLRWVEYX](/|-|-*>|→|-to-)[CISQMNPKDTFAGHLRWVEYX] *[-+]*\d+\b'),
-            re.compile(r'(?<!\w)[-+]*\d*:? *[CISQMNPKDTFAGHLRWVEYX] *(/|-|-*>|→|to|-to-) *[CISQMNPKDTFAGHLRWVEYX]\b'),
+            re.compile(r'(?<!\w)[-+]*\d*:? *[CISQMNPKDTFAGHLRWVEYX] *(/|-|-*>|→|-*to-*) *[CISQMNPKDTFAGHLRWVEYX]\b'),
 
             re.compile(r'(?<!\w)[-+]?\d+ *\d* *(b|bp|N|ntb|p|BP|B) *(INS|DEL|INDEL|DELINS|DUP|ins|del|indel|delins|dup)\b'),
             re.compile(r'(?<!\w)[-+]*\d+ *(INS|DEL|INDEL|DELINS|DUP|ins|del|indel|delins|dup)[0-9CISQMNPKDTFAGHLRWVEYX]+\b'),
