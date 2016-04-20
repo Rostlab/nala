81637475fd22be80f440bb2b63ffe99cdee58a72
Juan Miguel Cejuela <i@juanmi.rocks>
2016-04-20 18:27:20 +0200

    fix

--- scripts/getIAA.py
+++ scripts/getIAA.py
@@ -68,7 +68,7 @@ IDP4_IAA_docs = [
 def benchmark_IDP4(member1, member2):
     dataset = Dataset()
     itr = IterationRound(0)
-    dataset.extend_dataset(itr.read(read_annotations = False))
+    dataset.extend_dataset(itr.read(read_annotations=False))
 
     newcorpus = Dataset()
     for docid, document in dataset.documents.items():
@@ -77,30 +77,26 @@ def benchmark_IDP4(member1, member2):
 
     dataset = newcorpus
 
-    AnnJsonAnnotationReader(os.path.join(itr.path, "base", "tmVar_test", "members", member1), delete_incomplete_docs = True).annotate(dataset)
-    AnnJsonAnnotationReader(os.path.join(itr.path, "base", "tmVar_test", "members", member2), delete_incomplete_docs = True, is_predicted=True).annotate(dataset)
+    AnnJsonAnnotationReader(os.path.join(itr.path, "base", "annjson", "members", member1), delete_incomplete_docs=True).annotate(dataset)
+    AnnJsonAnnotationReader(os.path.join(itr.path, "base", "annjson", "members", member2), delete_incomplete_docs=True, is_predicted=True).annotate(dataset)
 
     print(dataset.__repr__())
 
     ExclusiveNLDefiner().define(dataset)
 
-    exact = MentionLevelEvaluator(strictness = 'exact', subclass_analysis=True).evaluate(dataset)
-    overlapping = MentionLevelEvaluator(strictness = 'overlapping', subclass_analysis=True).evaluate(dataset)
-    return exact, overlapping
+    return MentionLevelEvaluator(subclass_analysis=True).evaluate(dataset)
 
 def benchmark_nala(member1, member2):
     dataset = Dataset()
     for itr in IterationRound.all():
         if itr.is_IAA():
-            dataset.extend_dataset(itr.read(read_annotations = False))
-            AnnJsonAnnotationReader(os.path.join(itr.path, "reviewed", member1), delete_incomplete_docs = False).annotate(dataset)
-            AnnJsonAnnotationReader(os.path.join(itr.path, "reviewed", member2), delete_incomplete_docs = False, is_predicted=True).annotate(dataset)
+            dataset.extend_dataset(itr.read(read_annotations=False))
+            AnnJsonAnnotationReader(os.path.join(itr.path, "reviewed", member1), delete_incomplete_docs=False).annotate(dataset)
+            AnnJsonAnnotationReader(os.path.join(itr.path, "reviewed", member2), delete_incomplete_docs=False, is_predicted=True).annotate(dataset)
 
     ExclusiveNLDefiner().define(dataset)
 
-    exact = MentionLevelEvaluator(strictness = 'exact', subclass_analysis=True).evaluate(dataset)
-    overlapping = MentionLevelEvaluator(strictness = 'overlapping', subclass_analysis=True).evaluate(dataset)
-    return exact, overlapping
+    return MentionLevelEvaluator(subclass_analysis=True).evaluate(dataset)
 
 if sys.argv[1] == "IDP4":
     members = IDP4_members
@@ -111,11 +107,7 @@ else:
 
 for member1, member2 in combinations(members, 2):
     print(member1, member2)
-    exact, overlapping = benchmark(member1, member2)
+    evaluation = benchmark(member1, member2)
 
-    for e in exact:
-        print(e)
-    for e in overlapping:
-        print(e)
-
+    print(evaluation)
     print("")
