99b096ed043331d5ff77829c557d51a848d9bf44
Juan Miguel Cejuela <i@juanmi.rocks>
2016-05-25 19:45:55 +0200

    Change to be able to predict strings

--- scripts/SETH.py
+++ scripts/SETH.py
@@ -6,6 +6,7 @@ from nala.utils.corpora import get_corpus
 from nalaf.utils.annotation_readers import SETHAnnotationReader, BRATPartsAnnotationReader
 from nalaf.learning.evaluators import MentionLevelEvaluator
 from nala.preprocessing.definers import ExclusiveNLDefiner
+from nalaf.utils.readers import StringReader
 
 def run_seth_on_corpus(corpus, folder, useMutationFinderOnly):
     counter = 0
@@ -17,13 +18,20 @@ def run_seth_on_corpus(corpus, folder, useMutationFinderOnly):
             run_seth_on_string(part.text, docid, partid, folder, useMutationFinderOnly)
 
 def run_seth_on_string(text, docid, partid, folder, useMutationFinderOnly):
-    filename = "{}/{}-{}.ann".format(folder, docid, partid)
+    filename = "{}/{}-{}.ann".format(folder, docid, partid) if folder else None
     run_seth_on_string_with_filename(text, filename, useMutationFinderOnly)
 
 def run_seth_on_string_with_filename(text, filename, useMutationFinderOnly):
+    def run(output):
+        subprocess.run(["java", "seth.ner.wrapper.SETHNERAppMut", text, useMutationFinderOnly], stdout=output, stderr=subprocess.PIPE, universal_newlines=True, check=True)
+
     try:
-        with open(filename, "w") as outfile:
-            subprocess.run(["java", "seth.ner.wrapper.SETHNERAppMut", text, useMutationFinderOnly], stdout=outfile, stderr=subprocess.PIPE, universal_newlines=True, check=True)
+        if filename:
+            with open(filename, "w") as outfile:
+                run(outfile)
+        else:
+            run(None)
+
     except CalledProcessError as e:
         if "Error: Could not find or load main class seth.ner.wrapper.SETHNERAppMut" in e.stderr:
             raise Exception("Make sure to add seth.jar to your classpath (use repo https://github.com/juanmirocks/SETH) -- " + e.stderr)
@@ -34,14 +42,20 @@ methodName = sys.argv[1]
 assert methodName in {"SETH", "MFmodified", "check_performance"}, \
     "Method name must be SETH or MFmodified or check_performance"
 corpusName = sys.argv[2]
-corpus = get_corpus(corpusName)
-folderName = sys.argv[3]
+try:
+    corpus = get_corpus(corpusName)
+    folderName = sys.argv[3]
+except:
+    corpus = StringReader(corpusName).read()
+    folderName = None  # just print out in standard output
+
 
 if (methodName != 'check_performance'):
-    # Given predictions folder
-    folderName = os.path.join(folderName, methodName, corpusName)
-    if not os.path.exists(folderName):
-        os.makedirs(folderName)
+    if folderName:
+        # Given predictions folder
+        folderName = os.path.join(folderName, methodName, corpusName)
+        if not os.path.exists(folderName):
+            os.makedirs(folderName)
 
     useMutationFinderOnly = "true" if methodName == "MFmodified" else "false"
 
