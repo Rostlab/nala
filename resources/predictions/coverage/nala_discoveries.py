b11848a3062af7c18ccc9345f65107d9fce08171
Juan Miguel Cejuela <i@juanmi.rocks>
2016-10-07 16:21:36 +0200

    add script for calculating venn diagram

---
+++ resources/predictions/coverage/nala_discoveries.py
@@ -0,0 +1,27 @@
+
+# tmVar 3366cc
+A = set([
+
+])
+
+# SETH ffcc00
+B = set([
+
+])
+
+# nala 669933
+C = set([
+
+])
+
+# ------------------------------------------------
+
+A_SIZE = len(A)
+B_SIZE = len(B)
+C_SIZE = len(C)
+
+AiB_SIZE = len(A.intersection(B))
+AiC_SIZE = len(A.intersection(B))
+BiC_SIZE = len(B.intersection(C))
+
+AiBiC_SIZE = len(A.intersection(B).intersection(C))
