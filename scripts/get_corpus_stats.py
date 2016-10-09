5fd58b173553930987eafd92ce4c8ab6479e93e6
Juan Miguel Cejuela <i@juanmi.rocks>
2016-10-09 09:30:08 +0200

    minor

--- scripts/get_corpus_stats.py
+++ scripts/get_corpus_stats.py
@@ -180,7 +180,13 @@ def get_stats(name, corpus, typ):
 #
 # %d_u_NL == percentage of documents that have at least one untranslated NL mention
 # %m_u_NL == percentage of mentions that are NL and are untranslated to ST
-header = ["#Corpus", "#docs", "#tokens", "#ann", "#ST", "%ST", "#NL", "%NL", "#SS", "%SS", "#NL+SS", "%NL+SS", "%d_u_NL", "%m_u_NL"]
+header = [
+    "#Corpus",
+    "#docs",
+    "#tokens",
+    "#ann", "#ST", "%ST", "#NL", "%NL", "#SS", "%SS", "#NL+SS", "%NL+SS",
+    "%d_u_NL", "%m_u_NL"
+]
 
 print('\t'.join(header))
 
