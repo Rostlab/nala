a5cf190b70958156a0ea671efd1beba98e82614a
Juan Miguel Cejuela <i@juanmi.rocks>
2018-02-28 15:13:24 +0100

    add options for hdfs

--- nala/learning/train.py
+++ nala/learning/train.py
@@ -90,6 +90,9 @@ def train(argv):
     parser.add_argument('--keep_rs_ids', default='True',
                         help='Keep unnumbered mentions (default) or not, i.e., delete mentions like `rs1801280` or `ss221`')
 
+    parser.add_argument('--hdfs_url', required=False, type=str)
+    parser.add_argument('--hdfs_user', required=False, type=str)
+
     FALSE = ['false', 'f', '0', 'no', 'none']
 
     def arg_bool(arg_value):
@@ -194,10 +197,10 @@ def train(argv):
     definer = ExclusiveNLDefiner()
 
     if args.training_corpus:
-        train_set = get_corpus(args.training_corpus, args.only_class_id)
+        train_set = get_corpus(args.training_corpus, only_class_id=args.only_class_id, hdfs_url=args.hdfs_url, hdfs_user=args.hdfs_user)
 
         if args.test_corpus:
-            test_set = get_corpus(args.test_corpus, args.only_class_id)
+            test_set = get_corpus(args.test_corpus, only_class_id=args.only_class_id, hdfs_url=args.hdfs_url, hdfs_user=args.hdfs_user)
         elif args.string:
             test_set = StringReader(args.string).read()
         elif args.validation == "none":
