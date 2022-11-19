922eaa710a3b4680823d6a4159a06305140632e6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 00:46:54 +0000

    Adding tarfile member sanitization to extractall()

--- nala/utils/__init__.py
+++ nala/utils/__init__.py
@@ -163,7 +163,26 @@ def get_word_embeddings_feature_generator(model_location=None, additive=None, mu
                 print_verbose('Extracting')
 
                 with tarfile.open(we_model_tar_gz) as tar:
-                    tar.extractall(path=pkg_resources.resource_filename('nala.data', ''))
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, path=pkg_resources.resource_filename("nala.data",""))
 
             _SINGLETON_WE_GENERATOR = WordEmbeddingsFeatureGenerator(we_model, additive, multiplicative)
         else:
